7f6f6124e37812e9d719519d7013a1d9e87f2981
Jasand <jasand@vermilion.com>
2016-03-17 10:16:53 -0600

    add fields_variation field

--- src/SubPost.php
+++ src/SubPost.php
@@ -3,7 +3,8 @@
 class SubPost extends \Taco\Post {
   final public function getFields() {
     return array(
-      'field_assigned_to' => array('text')
+      'field_assigned_to' => array('text'),
+      'fields_variation' => array('type' => 'text')
     );
   }
   public function getPostTypeConfig() {
